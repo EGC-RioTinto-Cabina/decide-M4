dc9c16e7f8b6d56d5d9cefdbb76a5c77ca49ca52
lauporsol <lauraportillosoler@gmail.com>
2021-02-08 19:59:19 +0100

    #2 Cabina de votación si/no
    Cambios en views.py

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -58,8 +58,8 @@ class BoothView(TemplateView):
             raise Http404
         context['KEYBITS'] = settings.KEYBITS
         
-        context['start_date'] = self.format_fecha(context['start_date'])
-        context['end_date'] = self.format_fecha(context['end_date'])
+   '''     context['start_date'] = self.format_fecha(context['start_date'])
+        context['end_date'] = self.format_fecha(context['end_date'])'''
         
         return context
     
